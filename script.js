7dfdbf7cdab59831998c40dd0248aa5eae2d3814
Dicky Dwi Putra <dickydwiputra.17@gmail.com>
2021-11-24 22:06:40 +0700

    okeyy

---
+++ script.js
@@ -0,0 +1,16 @@
+var interval = setInterval(timestamphome, 1000);
+
+
+
+
+ function timestamphome(){
+ var date;
+date = new Date();
+
+ 
+ var time = document.getElementById('timediv'); 
+ time.innerHTML = date.toLocaleTimeString();
+
+
+  }
+ 
